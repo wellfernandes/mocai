4738f4856e8ead269133b875a1d2582b2b164863
Welliton Fernandes Leal <wellfernandes@me.com>
2025-01-29 18:02:24 -0300

    fix: fixed person generation to make use of translations

--- pkg/mocai/entities/person/generator.go
+++ pkg/mocai/entities/person/generator.go
@@ -3,9 +3,10 @@ package person
 import (
 	"fmt"
 	"math/rand"
+	"strings"
 
 	"github.com/brazzcore/mocai/pkg/mocai/constants"
-	mocks "github.com/brazzcore/mocai/pkg/mocai/mocks/pt_br"
+	"github.com/brazzcore/mocai/pkg/mocai/translations"
 )
 
 // Person represents a mock person with first names, last name, and age.
@@ -19,20 +20,28 @@ type Person struct {
 // GeneratePerson generates a mock person with random data.
 // It returns a pointer to a Person and an error if the generation fails.
 func GeneratePerson() (*Person, error) {
+	lang := translations.GetLanguage()
+
+	// Get the list of first names and last names
+	firstNamesMale := strings.Split(translations.Get(lang, "person_first_name_male"), ",")
+	firstNamesFemale := strings.Split(translations.Get(lang, "person_first_name_female"), ",")
+	lastNames := strings.Split(translations.Get(lang, "person_last_name"), ",")
+
 	// Validate data
-	if len(mocks.FirstNamesMale) == 0 || len(mocks.FirstNamesFemale) == 0 {
+	if len(firstNamesMale) == 0 || len(firstNamesFemale) == 0 {
 		return nil, fmt.Errorf(constants.ERROR_NO_FIRST_NAMES)
 	}
 
-	if len(mocks.LastNames) == 0 {
+	if len(lastNames) == 0 {
 		return nil, fmt.Errorf(constants.ERROR_NO_LAST_NAMES)
 	}
 
-	// Choose random values from constants
-	firstNameMale := mocks.FirstNamesMale[rand.Intn(len(mocks.FirstNamesMale))]
-	firstNameFemale := mocks.FirstNamesFemale[rand.Intn(len(mocks.FirstNamesFemale))]
-	lastName := mocks.LastNames[rand.Intn(len(mocks.LastNames))]
+	// Choose random values
+	firstNameMale := firstNamesMale[rand.Intn(len(firstNamesMale))]
+	firstNameFemale := firstNamesFemale[rand.Intn(len(firstNamesFemale))]
+	lastName := lastNames[rand.Intn(len(lastNames))]
 
+	// Validate required fields
 	if firstNameMale == "" || firstNameFemale == "" || lastName == "" {
 		return nil, fmt.Errorf("%s: missing required data (firstNameMale: %s, firstNameFemale: %s, lastName: %s)",
 			constants.ERROR_GENERATING_PERSON, firstNameMale, firstNameFemale, lastName)
@@ -46,5 +55,4 @@ func GeneratePerson() (*Person, error) {
 	}
 
 	return createdPerson, nil
-
 }
