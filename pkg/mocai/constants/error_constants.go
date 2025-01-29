176846118c6d0ea1b1ef5c4ed926548cb209a092
Welliton Fernandes Leal <wellfernandes@me.com>
2025-01-29 17:37:24 -0300

    feat: added uf error constant

--- pkg/mocai/constants/error_constants.go
+++ pkg/mocai/constants/error_constants.go
@@ -10,5 +10,6 @@ const (
 	ERROR_NO_STREETS         = "no data available for streets"
 	ERROR_NO_CITIES          = "no data available for cities"
 	ERROR_NO_STATES          = "no data available for states"
+	ERROR_NO_UFS             = "no data available for UFs"
 	ERROR_NO_ZIPS            = "no data available for zips"
 )
