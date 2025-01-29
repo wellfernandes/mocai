11234aa1099fb01ae21c08ce13e82cc856c164fc
Welliton Fernandes Leal <wellfernandes@me.com>
2025-01-29 19:31:31 -0300

    feat: added unsupported language error

--- pkg/mocai/constants/error_constants.go
+++ pkg/mocai/constants/error_constants.go
@@ -4,15 +4,16 @@ package constants
 
 // Error messages for mock data generation operations.
 const (
-	ERROR_GENERATING_ADDRESS = "error generating address"
-	ERROR_GENERATING_PERSON  = "error generating person"
-	ERROR_GENERATING_PHONE   = "error generating phone"
-	ERROR_NO_AREA_CODES      = "no data available for area codes"
-	ERROR_NO_FIRST_NAMES     = "no data available for first names"
-	ERROR_NO_LAST_NAMES      = "no data available for last names"
-	ERROR_NO_STREETS         = "no data available for streets"
-	ERROR_NO_CITIES          = "no data available for cities"
-	ERROR_NO_STATES          = "no data available for states"
-	ERROR_NO_UFS             = "no data available for UFs"
-	ERROR_NO_ZIPS            = "no data available for zips"
+	ERROR_UNSUPPORTED_LANGUAGE = "unsupported language"
+	ERROR_GENERATING_ADDRESS   = "error generating address"
+	ERROR_GENERATING_PERSON    = "error generating person"
+	ERROR_GENERATING_PHONE     = "error generating phone"
+	ERROR_NO_AREA_CODES        = "no data available for area codes"
+	ERROR_NO_FIRST_NAMES       = "no data available for first names"
+	ERROR_NO_LAST_NAMES        = "no data available for last names"
+	ERROR_NO_STREETS           = "no data available for streets"
+	ERROR_NO_CITIES            = "no data available for cities"
+	ERROR_NO_STATES            = "no data available for states"
+	ERROR_NO_UFS               = "no data available for UFs"
+	ERROR_NO_ZIPS              = "no data available for zips"
 )
