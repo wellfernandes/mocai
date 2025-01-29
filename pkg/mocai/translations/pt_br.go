774bfad7d2c18baa5253896efc660eead7c5a3f3
Welliton Fernandes Leal <wellfernandes@me.com>
2025-01-29 17:48:54 -0300

    fix: fixed generation of uf by state

--- pkg/mocai/translations/pt_br.go
+++ pkg/mocai/translations/pt_br.go
@@ -7,13 +7,20 @@ import (
 )
 
 func init() {
+	// Choose a random state
+	state := mocks.States[rand.Intn(len(mocks.States))]
+
+	// Gets the UF corresponding to the selected state
+	uf := mocks.UFs[state]
+
 	Register("pt", map[string]string{
 		"person_first_name_male":   mocks.FirstNamesMale[rand.Intn(len(mocks.FirstNamesMale))],
 		"person_first_name_female": mocks.FirstNamesFemale[rand.Intn(len(mocks.FirstNamesFemale))],
 		"person_last_name":         mocks.LastNames[rand.Intn(len(mocks.LastNames))],
 		"address_street":           mocks.Streets[rand.Intn(len(mocks.Streets))],
 		"address_city":             mocks.Cities[rand.Intn(len(mocks.Cities))],
-		"address_state":            mocks.States[rand.Intn(len(mocks.States))],
+		"address_state":            state,
+		"address_uf":               uf,
 		"address_zip":              mocks.ZIPCodes[rand.Intn(len(mocks.ZIPCodes))],
 		"phone_area_code":          mocks.AreaCodes[rand.Intn(len(mocks.AreaCodes))],
 	})
