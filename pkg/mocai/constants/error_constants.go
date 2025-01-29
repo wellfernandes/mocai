ad7b4e33bd50e2738267ce51aeda21ebe02a81bc
Welliton Fernandes Leal <wellfernandes@me.com>
2025-01-29 16:58:10 -0300

    feat: changed to snake case pattern and added new error constants

--- pkg/mocai/constants/error_constants.go
+++ pkg/mocai/constants/error_constants.go
@@ -1,7 +1,14 @@
 package constants
 
 const (
-	ErrorGeneratingAddress = "error generating address"
-	ErrorGeneratingPerson  = "error generating person"
-	ErrorGeneratingPhone   = "error generating phone"
+	ERROR_GENERATING_ADDRESS = "error generating address"
+	ERROR_GENERATING_PERSON  = "error generating person"
+	ERROR_GENERATING_PHONE   = "error generating phone"
+	ERROR_NO_AREA_CODES      = "no data available for area codes"
+	ERROR_NO_FIRST_NAMES     = "no data available for first names"
+	ERROR_NO_LAST_NAMES      = "no data available for last names"
+	ERROR_NO_STREETS         = "no data available for streets"
+	ERROR_NO_CITIES          = "no data available for cities"
+	ERROR_NO_STATES          = "no data available for states"
+	ERROR_NO_ZIPS            = "no data available for zips"
 )
