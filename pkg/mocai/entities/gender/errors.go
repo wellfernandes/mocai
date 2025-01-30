2453fd3ec1fb19c1f0927e03f747c25cc1027407
Welliton Fernandes Leal <wellfernandes@me.com>
2025-01-30 14:35:59 -0300

    feat: added error for gender entity

---
+++ pkg/mocai/entities/gender/errors.go
@@ -0,0 +1,10 @@
+package gender
+
+import "errors"
+
+// Package gender defines common errors used during gender generation and validation.
+// These errors represent specific failure scenarios and should be wrapped with additional
+// context when returned.
+var (
+	ErrNoGenders = errors.New("no data available for genders")
+)
