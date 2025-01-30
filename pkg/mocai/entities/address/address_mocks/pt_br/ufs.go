e51c3aedd96bbced5d2a19fb2475238c4dc66860
Welliton Fernandes Leal <wellfernandes@me.com>
2025-01-30 08:50:37 -0300

    refactor: mock constants moved to their respective entity

---
+++ pkg/mocai/entities/address/address_mocks/pt_br/ufs.go
@@ -0,0 +1,32 @@
+package address_mocks
+
+// UF maps Brazilian state names to their respective UFs (Unidades Federativas).
+var UFs = map[string]string{
+	"Acre":                "AC",
+	"Alagoas":             "AL",
+	"Amapá":               "AP",
+	"Amazonas":            "AM",
+	"Bahia":               "BA",
+	"Ceará":               "CE",
+	"Distrito Federal":    "DF",
+	"Espírito Santo":      "ES",
+	"Goiás":               "GO",
+	"Maranhão":            "MA",
+	"Mato Grosso":         "MT",
+	"Mato Grosso do Sul":  "MS",
+	"Minas Gerais":        "MG",
+	"Pará":                "PA",
+	"Paraíba":             "PB",
+	"Paraná":              "PR",
+	"Pernambuco":          "PE",
+	"Piauí":               "PI",
+	"Rio de Janeiro":      "RJ",
+	"Rio Grande do Norte": "RN",
+	"Rio Grande do Sul":   "RS",
+	"Rondônia":            "RO",
+	"Roraima":             "RR",
+	"Santa Catarina":      "SC",
+	"São Paulo":           "SP",
+	"Sergipe":             "SE",
+	"Tocantins":           "TO",
+}
