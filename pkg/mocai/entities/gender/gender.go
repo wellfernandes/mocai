74b5441053c1e4d0dbd421aba65c4d4057379e4e
Welliton Fernandes Leal <wellfernandes@me.com>
2025-01-30 14:15:04 -0300

    feat: added gender entity

---
+++ pkg/mocai/entities/gender/gender.go
@@ -0,0 +1,24 @@
+package gender
+
+import "github.com/brazzcore/mocai/pkg/mocai/translations"
+
+// Gender represents a person's gender identity.
+type Gender string
+
+// Constants for different gender identities.
+const (
+	Male        Gender = "male"        // Male gender identity
+	Female      Gender = "female"      // Female gender identity
+	NonBinary   Gender = "non-binary"  // Non-binary gender identity
+	GenderFluid Gender = "genderfluid" // Genderfluid identity
+	Agender     Gender = "agender"     // Agender identity
+	TwoSpirit   Gender = "two-spirit"  // Two-Spirit identity (used by some Indigenous peoples)
+	Other       Gender = "other"       // Other gender identity
+)
+
+// GenerateRandomGender generates a random gender based on the current language.
+func GenerateRandomGender() Gender {
+	lang := translations.GetLanguage()
+	genderStr := translations.Get(lang, "gender")
+	return Gender(genderStr)
+}
