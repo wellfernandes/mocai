77c1c4c3be02f0628e620991e889bd10bf666603
Welliton Fernandes Leal <wellfernandes@me.com>
2025-02-05 08:01:18 -0300

    fix: fixed urls

--- mocai-docs/docusaurus.config.js
+++ mocai-docs/docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://github.com',
+  url: 'https://brazzcore.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/mocai/',
@@ -89,7 +89,7 @@ const config = {
             items: [
               {
                 label: 'Tutorial',
-                to: '/docs/intro',
+                to: '/docs/tutorial',
               },
               {
                 label: 'Tasks',
