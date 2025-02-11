26aff367ab576e0b8c2d1e69862f58a4d212fcad
Nikul <nikulpanchal@192.168.1.20>
2025-02-11 18:04:33 +0530

    :sparkles: feat: Adds moch adhar card and pancard functionality for enin

--- pkg/mocai/helper/helper.go
+++ pkg/mocai/helper/helper.go
@@ -8,11 +8,12 @@ import (
 
 // Generate a random Aadhaar number
 func GenerateAadhaarNumber() string {
-	rand.Seed(time.Now().UnixNano())
+	src := rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(src)
 	aadhaar := ""
 
 	for i := 0; i < 12; i++ {
-		aadhaar += strconv.Itoa(rand.Intn(10))
+		aadhaar += strconv.Itoa(rng.Intn(10))
 	}
 	return aadhaar
 }
@@ -21,21 +22,23 @@ func GenerateAadhaarNumber() string {
 func GenerateRandomPAN() string {
 	var letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	var digits = "0123456789"
-	rand.Seed(time.Now().UnixNano())
+	// Create a new source and random generator
+	src := rand.NewSource(time.Now().UnixNano())
+	rng := rand.New(src)
 
 	// Generate first 5 letters
 	pan := ""
 	for i := 0; i < 5; i++ {
-		pan += string(letters[rand.Intn(len(letters))])
+		pan += string(letters[rng.Intn(len(letters))])
 	}
 
 	// Generate 4 digits
 	for i := 0; i < 4; i++ {
-		pan += string(digits[rand.Intn(len(digits))])
+		pan += string(digits[rng.Intn(len(digits))])
 	}
 
 	// Generate last letter
-	pan += string(letters[rand.Intn(len(letters))])
+	pan += string(letters[rng.Intn(len(letters))])
 
 	return pan
 }
