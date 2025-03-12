44096d03dfc62bedcc419c8d68d9b7b999adc32d
Welliton Fernandes Leal <wellfernandes@me.com>
2025-03-12 15:02:11 -0300

    refactor: updated company generation example

--- examples/mock_example.go
+++ examples/mock_example.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/brazzcore/mocai/pkg/mocai/constants"
 	"github.com/brazzcore/mocai/pkg/mocai/entities/address"
-	company "github.com/brazzcore/mocai/pkg/mocai/entities/company/brazil"
+	"github.com/brazzcore/mocai/pkg/mocai/entities/company"
 	"github.com/brazzcore/mocai/pkg/mocai/entities/person"
 	"github.com/brazzcore/mocai/pkg/mocai/entities/phone"
 	"github.com/brazzcore/mocai/pkg/mocai/translations"
@@ -33,8 +33,7 @@ func GenerateMockExample() {
 
 	company_mock, err := company.GenerateCompany()
 	if err != nil {
-		fmt.Println("Error generating company:", err)
-		return
+		fmt.Print(err)
 	}
 
 	fmt.Println(constants.HeaderMain)
@@ -43,7 +42,7 @@ func GenerateMockExample() {
 	fmt.Printf("Person: %s %s, %s, %d years old, CPF: %s\n",
 		person_mock.FirstNameMale, person_mock.LastName, person_mock.Gender, person_mock.Age, person_mock.CPF)
 
-	fmt.Printf("Company: %s, CNPJ: %s\n", company_mock.CompanyName, company_mock.CNPJ)
+	fmt.Printf("Company: %s, CNPJ: %s\n", company_mock.Name(), company_mock.RegistrationNumber())
 
 	fmt.Printf("Address: %s, %d - %s, %s (%s) - %s\n",
 		address_mock.Street, address_mock.Number, address_mock.City, address_mock.State, address_mock.UF, address_mock.ZIP)
