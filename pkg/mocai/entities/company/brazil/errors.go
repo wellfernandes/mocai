18f1720d7434a2641421070393fca47031046709
Welliton Fernandes Leal <wellfernandes@me.com>
2025-03-12 14:49:42 -0300

    remove: removed brazil directory

--- pkg/mocai/entities/company/brazil/errors.go
+++
@@ -1,8 +0,0 @@
-package company
-
-// Error constants for person-related data generation failures.
-// These errors represent specific failure scenarios and should be wrapped with additional
-// context when returned.
-const (
-	ERROR_GENERATING_BRAZILIAN_COMPANY = "error generating brazilian company"
-)
