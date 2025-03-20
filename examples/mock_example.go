08e1a8e8622ef40ac7d96a3e6709fe18440d8c35
Welliton Fernandes Leal <wellfernandes@me.com>
2025-03-20 20:12:10 -0300

    feat: added example for birth certificate

--- examples/mock_example.go
+++ examples/mock_example.go
@@ -5,8 +5,8 @@ import (
 
 	"github.com/brazzcore/mocai/pkg/mocai/constants"
 	"github.com/brazzcore/mocai/pkg/mocai/entities/address"
-	brazilian_birth_certificate "github.com/brazzcore/mocai/pkg/mocai/entities/birth_certificate/brazil"
-	brazilian_company "github.com/brazzcore/mocai/pkg/mocai/entities/company/brazil"
+	"github.com/brazzcore/mocai/pkg/mocai/entities/birth_certificate"
+	"github.com/brazzcore/mocai/pkg/mocai/entities/company"
 	"github.com/brazzcore/mocai/pkg/mocai/entities/person"
 	"github.com/brazzcore/mocai/pkg/mocai/entities/phone"
 	"github.com/brazzcore/mocai/pkg/mocai/translations"
@@ -32,13 +32,13 @@ func GenerateMockExample() {
 		fmt.Print(err)
 	}
 
-	company_mock, err := brazilian_company.GenerateCompany()
+	company_mock, err := company.GenerateCompany(false)
 	if err != nil {
 		fmt.Print(err)
 		return
 	}
 
-	birth_certificate_mock, err := brazilian_birth_certificate.GenerateBirthCertificate(false)
+	birth_certificate_mock, err := birth_certificate.GenerateBirthCertificate(false)
 	if err != nil {
 		fmt.Print(err)
 		return
@@ -50,9 +50,9 @@ func GenerateMockExample() {
 	fmt.Printf("Person: %s %s, %s, %d years old, CPF: %s\n",
 		person_mock.FirstNameMale, person_mock.LastName, person_mock.Gender, person_mock.Age, person_mock.CPF)
 
-	fmt.Printf("Birth Certificate: %s\n", birth_certificate_mock.BirthCertificateNumber)
+	fmt.Printf("Birth Certificate: %s\n", birth_certificate_mock.BrazilianBirthCertificate.CertificateNumber)
 
-	fmt.Printf("Company: %s, CNPJ: %s\n", company_mock.CompanyName, company_mock.CNPJ)
+	fmt.Printf("Company: %s, CNPJ: %s\n", company_mock.BrazilianCompany.CompanyName, company_mock.BrazilianCompany.CNPJ)
 
 	fmt.Printf("Address: %s, %d - %s, %s (%s) - %s\n",
 		address_mock.Street, address_mock.Number, address_mock.City, address_mock.State, address_mock.UF, address_mock.ZIP)
