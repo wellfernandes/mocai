343485f53ede0785696321e7e63349d0f8e20ad7
Welliton Fernandes Leal <wellfernandes@me.com>
2025-03-20 20:10:29 -0300

    remove: folders deleted after restructuring packages

--- pkg/mocai/generator/interface.go
+++
@@ -1,5 +0,0 @@
-package generator
-
-// Generator is an interface that defines the signature of a data generation function.
-// It returns a value of type `interface{}`, allowing flexibility in the type of data generated.
-type Generator func() interface{}
