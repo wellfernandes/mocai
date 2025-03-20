d60e0bb5a63b28b2b8aa2520b6c705eaf9b07d04
Welliton Fernandes Leal <wellfernandes@me.com>
2025-03-20 16:22:17 -0300

    fix: fixed comments for company errors

--- pkg/mocai/entities/company/countries/errors.go
+++ pkg/mocai/entities/company/countries/errors.go
@@ -2,7 +2,7 @@ package countries
 
 import "errors"
 
-// Error constants for Brazilian company data generation failures.
+// Error constants for Company data generation failures.
 // These errors represent specific failure scenarios and should be wrapped with additional
 // context when returned.
 var (
