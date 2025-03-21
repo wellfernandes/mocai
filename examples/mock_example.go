14d5527cdd648183c0a998a1feced117c23c0727
Welliton Fernandes Leal <wellfernandes@me.com>
2025-03-21 18:43:48 -0300

    feat: added mock example for voter registration

--- examples/mock_example.go
+++ examples/mock_example.go
@@ -9,6 +9,7 @@ import (
 	"github.com/brazzcore/mocai/pkg/mocai/entities/company"
 	"github.com/brazzcore/mocai/pkg/mocai/entities/person"
 	"github.com/brazzcore/mocai/pkg/mocai/entities/phone"
+	"github.com/brazzcore/mocai/pkg/mocai/entities/vote_registration"
 	"github.com/brazzcore/mocai/pkg/mocai/translations"
 )
 
@@ -44,6 +45,12 @@ func GenerateMockExample() {
 		return
 	}
 
+	vote_registration_mock, err := vote_registration.GenerateVoteRegistration(false)
+	if err != nil {
+		fmt.Print(err)
+		return
+	}
+
 	fmt.Println(constants.HeaderMain)
 	fmt.Println(constants.SubHeader)
 
@@ -61,6 +68,8 @@ func GenerateMockExample() {
 	fmt.Printf("Address: %s, %d - %s, %s (%s) - %s\n",
 		address_mock.Street, address_mock.Number, address_mock.City, address_mock.State, address_mock.UF, address_mock.ZIP)
 	fmt.Printf("Phone: (%s) %s\n", phone_mock.AreaCode, phone_mock.Number)
+
+	fmt.Printf("Voter Registration Card: Section: %s, Zone: %s, Registration: %s\n", vote_registration_mock.BrazilianVoteRegistration.Section, vote_registration_mock.BrazilianVoteRegistration.Zone, vote_registration_mock.BrazilianVoteRegistration.Number)
 
 	fmt.Println(constants.Footer)
 }
