d97c13d7c7c77e9916d4d0f9438f9f4f1fb65216
Welliton Fernandes Leal <wellfernandes@me.com>
2025-03-21 18:54:00 -0300

    docs: added comments on voter registration errors

--- pkg/mocai/entities/vote_registration/countries/errors.go
+++ pkg/mocai/entities/vote_registration/countries/errors.go
@@ -2,6 +2,9 @@ package countries
 
 import "errors"
 
+// Errors for Vote Registration data generation failures.
+// These errors represent specific failure scenarios and should be wrapped with additional
+// context when returned.
 var (
 	ErrInvalidVoteRegistration = errors.New("invalid vote registration")
 )
