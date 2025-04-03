ac77ba0ee883777d327f7ae3583c133b52743ffc
Welliton Fernandes Leal <wellfernandes@me.com>
2025-04-03 16:15:10 -0300

    refactor: removed unused error

--- pkg/mocai/entities/national_id/countries/errors.go
+++ pkg/mocai/entities/national_id/countries/errors.go
@@ -4,6 +4,5 @@ import "errors"
 
 var (
 	ErrGeneratingBrazilianNationalID = errors.New("error generating brazilian national id")
-	ErrBaseNumberExceedsLimit        = errors.New("base number exceeds 8 digits limit")
 	ErrToConvertDigit                = errors.New("error converting digit")
 )
