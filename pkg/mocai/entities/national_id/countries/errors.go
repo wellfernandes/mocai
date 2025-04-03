0d42533f3477f23a89402fb23ff4584b1228b716
Welliton Fernandes Leal <wellfernandes@me.com>
2025-04-03 13:10:28 -0300

    feat: added national id errors

--- pkg/mocai/entities/national_id/countries/errors.go
+++ pkg/mocai/entities/national_id/countries/errors.go
@@ -4,4 +4,6 @@ import "errors"
 
 var (
 	ErrGeneratingBrazilianNationalID = errors.New("error generating brazilian national id")
+	ErrBaseNumberExceedsLimit        = errors.New("base number exceeds 8 digits limit")
+	ErrToConvertDigit                = errors.New("error converting digit")
 )
