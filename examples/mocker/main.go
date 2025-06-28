bbd88b68cca3e13e8c9769e04221e550f251d003
Welliton Fernandes Leal <wellfernandes@me.com>
2025-06-28 13:50:46 -0300

    feat: added simplified usage example

---
+++ examples/mocker/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+
+	cli "github.com/brazzcore/mocai/cmd/mocai"
+	"github.com/brazzcore/mocai/pkg/mocai"
+)
+
+func main() {
+
+	fmt.Println(cli.HeaderMain)
+	fmt.Println(cli.SubHeader)
+
+	// Initialize mocai and set a supported language
+	// If the language is not available, the default language pt-BR will be set
+	m := mocai.NewMocker("ptbr")
+
+	// Generate mock data
+	// You can select which entity attributes to use
+	// by accessing the entity and its fields
+	m.Person()  // m.Person().Age, m.Person().CPF...
+	m.Address() // m.Address().City, m.Address().Number...
+	m.Phone()   // m.Phone().AreaCode, m.Phone().Number
+
+	// Printing mocks example
+	addr := m.Address()
+	fmt.Printf("Address: %s, %d - %s, %s (%s) - %s\n",
+		addr.Street, addr.Number, addr.City, addr.State, addr.UF, addr.ZIP)
+
+	fmt.Println(cli.Footer)
+}
