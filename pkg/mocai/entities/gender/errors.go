5b6530f11a0a4b38f8793a8e29751afa618f47fc
Welliton Fernandes Leal <wellfernandes@me.com>
2025-10-17 23:35:00 -0300

    feat: added new error invalid gender

--- pkg/mocai/entities/gender/errors.go
+++ pkg/mocai/entities/gender/errors.go
@@ -6,5 +6,6 @@ import "errors"
 // These errors represent specific failure scenarios and should be wrapped with additional
 // context when returned.
 var (
-	ErrNoGenders = errors.New("no data available for genders")
+	ErrNoGenders     = errors.New("no data available for genders")
+	ErrInvalidGender = errors.New("invalid gender")
 )
