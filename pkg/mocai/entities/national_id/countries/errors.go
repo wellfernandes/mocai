45b564968078c5706b9e86f706459c6323fc1133
Welliton Fernandes Leal <wellfernandes@me.com>
2025-10-17 23:25:58 -0300

    remove: packages and files removed after name changes

--- pkg/mocai/entities/national_id/countries/errors.go
+++
@@ -1,8 +0,0 @@
-package countries
-
-import "errors"
-
-var (
-	ErrGeneratingBrazilianNationalID = errors.New("error generating brazilian national id")
-	ErrToConvertDigit                = errors.New("error converting digit")
-)
