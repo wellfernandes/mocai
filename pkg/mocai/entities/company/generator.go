f36b01e54aa4a51046d3fe1e9e04c1397e4d722a
Welliton Fernandes Leal <wellfernandes@me.com>
2025-10-17 23:33:24 -0300

    refactor: added constructor for companies

--- pkg/mocai/entities/company/generator.go
+++ pkg/mocai/entities/company/generator.go
@@ -4,15 +4,24 @@ import "github.com/brazzcore/mocai/pkg/mocai/entities/company/countries"
 
 // Company represents a generic company.
 type Company struct {
-	BrazilianCompany *countries.BrazilianCompany
+	BrazilianCompany countries.BrazilianCompany
+}
+
+// NewCompany creates a new Company with generated data.
+func NewCompany(isFormatted bool) Company {
+	comp, err := (&Company{}).generateCompany(isFormatted)
+	if err != nil {
+		return Company{}
+	}
+	return comp
 }
 
 // GenerateCompany generates all companies available.
-func GenerateCompany(formatted bool) (*Company, error) {
+func (c *Company) generateCompany(formatted bool) (Company, error) {
 	createdCompanyBrazilian, err := countries.GenerateBrazilianCompany(formatted)
 	if err != nil {
-		return nil, err
+		return Company{}, err
 	}
 
-	return &Company{BrazilianCompany: createdCompanyBrazilian}, nil
+	return Company{BrazilianCompany: createdCompanyBrazilian}, nil
 }
