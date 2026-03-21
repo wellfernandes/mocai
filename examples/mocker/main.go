b3917c0c40a07ff77ebbb4e7eb2f2027aac75472
Welliton Fernandes Leal <wellfernandes@me.com>
2026-03-21 18:24:28 -0300

    docs(example): adjusted mocai usage examples

--- examples/mocker/main.go
+++ examples/mocker/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/brazzcore/mocai/internal/cli"
 	"github.com/brazzcore/mocai/pkg/mocai"
@@ -13,17 +12,18 @@ func main() {
 	fmt.Println(cli.HeaderMain)
 	fmt.Println(cli.SubHeader)
 
-	// Initialize mocai and set a supported language
-	// If the language is not available, the default language pt-BR will be set
-	m, err := mocai.NewMocker("ptbr", true)
+	m := mocai.NewMocker("ptbr", true, nil)
+
+	address, err := m.NewAddress()
 	if err != nil {
-		log.Println(err)
+		fmt.Println(err)
+	} else {
+		fmt.Printf("Address: %s, %d - %s, %s (%s) - %s\n",
+			address.Street, address.Number, address.City, address.State, address.UF, address.ZIP)
 	}
-	// Generate mock data
-	// You can select which entity attributes to use
-	// by accessing the entity and its fields
-	fmt.Printf("Address: %s, %d - %s, %s (%s) - %s\n",
-		m.Address.Street, m.Address.Number, m.Address.City, m.Address.State, m.Address.UF, m.Address.ZIP)
+
+	certificate, _ := m.NewCertificate()
+	fmt.Println("Brazilian birth certificate:", certificate.Brazil.BirthCertificate.Number)
 
 	fmt.Println(cli.Footer)
 }
