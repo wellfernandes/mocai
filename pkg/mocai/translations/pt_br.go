2e96812fbbe455843545298445f5987085108464
Welliton Fernandes Leal <wellfernandes@me.com>
2026-03-21 18:04:25 -0300

    refactor: adjusted ptbr translation list

--- pkg/mocai/translations/pt_br.go
+++ pkg/mocai/translations/pt_br.go
@@ -11,24 +11,57 @@ import (
 )
 
 func init() {
-	// Choose a random state
+	// register person names lists for person_first_name_male, person_first_name_female, person_last_name
+	RegisterList("ptbr", map[string][]string{
+		"person_first_name_male":   person_mocks.FirstNamesMale,
+		"person_first_name_female": person_mocks.FirstNamesFemale,
+		"person_last_name":         person_mocks.LastNames,
+	})
+
+	// register gender list for gender key
+	RegisterList("ptbr", map[string][]string{
+		"gender": gender_mocks.Genders,
+	})
+
+	// register company names list for company_name key
+	RegisterList("ptbr", map[string][]string{
+		"company_name": company_mocks.CompanyNames,
+	})
+
+	// registers complete lists for use in mocks
+	RegisterList("ptbr", map[string][]string{
+		"address_street": address_mocks.Streets,
+		"address_city":   address_mocks.Cities,
+		"address_state":  address_mocks.States,
+		"address_uf": func() []string {
+			ufs := make([]string, 0, len(address_mocks.UFs))
+			for _, uf := range address_mocks.UFs {
+				ufs = append(ufs, uf)
+			}
+			return ufs
+		}(),
+		"address_zip": address_mocks.ZIPCodes,
+	})
+	// choose a random state
 	state := address_mocks.States[rand.Intn(len(address_mocks.States))]
 
-	// Gets the UF corresponding to the selected state
+	// gets the UF corresponding to the selected state
 	uf := address_mocks.UFs[state]
 
 	Register("ptbr", map[string]string{
-		"person_first_name_male":   person_mocks.FirstNamesMale[rand.Intn(len(person_mocks.FirstNamesMale))],
-		"person_first_name_female": person_mocks.FirstNamesFemale[rand.Intn(len(person_mocks.FirstNamesFemale))],
-		"person_last_name":         person_mocks.LastNames[rand.Intn(len(person_mocks.LastNames))],
-		"gender":                   gender_mocks.Genders[rand.Intn(len(gender_mocks.Genders))],
-		"address_street":           address_mocks.Streets[rand.Intn(len(address_mocks.Streets))],
-		"address_city":             address_mocks.Cities[rand.Intn(len(address_mocks.Cities))],
-		"address_state":            state,
-		"address_uf":               uf,
-		"address_zip":              address_mocks.ZIPCodes[rand.Intn(len(address_mocks.ZIPCodes))],
-		"phone_area_code":          phone_mocks.AreaCodes[rand.Intn(len(phone_mocks.AreaCodes))],
-		"company_name":             company_mocks.CompanyNames[rand.Intn(len(company_mocks.CompanyNames))],
+		"brazilian_rg_state":        "SP",
+		"brazilian_rg_issuing_body": "SSP - Secretaria de Segurança Pública",
+		"person_first_name_male":    person_mocks.FirstNamesMale[rand.Intn(len(person_mocks.FirstNamesMale))],
+		"person_first_name_female":  person_mocks.FirstNamesFemale[rand.Intn(len(person_mocks.FirstNamesFemale))],
+		"person_last_name":          person_mocks.LastNames[rand.Intn(len(person_mocks.LastNames))],
+		"gender":                    gender_mocks.Genders[rand.Intn(len(gender_mocks.Genders))],
+		"address_street":            address_mocks.Streets[rand.Intn(len(address_mocks.Streets))],
+		"address_city":              address_mocks.Cities[rand.Intn(len(address_mocks.Cities))],
+		"address_state":             state,
+		"address_uf":                uf,
+		"address_zip":               address_mocks.ZIPCodes[rand.Intn(len(address_mocks.ZIPCodes))],
+		"phone_area_code":           phone_mocks.AreaCodes[rand.Intn(len(phone_mocks.AreaCodes))],
+		"company_name":              company_mocks.CompanyNames[rand.Intn(len(company_mocks.CompanyNames))],
 
 		// errors
 		"invalid_certificate":                  "certidão inválida",
