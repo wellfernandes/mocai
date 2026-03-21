afaca8951bb0507b1e149035353f50992e0ec695
Welliton Fernandes Leal <wellfernandes@me.com>
2026-03-21 17:56:26 -0300

    refactor(person): adjusted person generation

--- pkg/mocai/entities/person/generator.go
+++ pkg/mocai/entities/person/generator.go
@@ -3,14 +3,13 @@ package person
 import (
 	"fmt"
 	"math/rand"
-	"strings"
 
 	"github.com/brazzcore/mocai/pkg/mocai/entities/cpf"
 	"github.com/brazzcore/mocai/pkg/mocai/entities/gender"
 	"github.com/brazzcore/mocai/pkg/mocai/translations"
 )
 
-// Person represents a mock person with first names, last name, and age.
+// Person represents a mock person with first names, last name, and age
 type Person struct {
 	FirstNameMale   string
 	FirstNameFemale string
@@ -20,61 +19,52 @@ type Person struct {
 	CPF             *cpf.CPF
 }
 
-func NewPerson(isFormatted bool) (*Person, error) {
-	p, err := (&Person{}).generatePerson(isFormatted)
-	if err != nil {
-		return nil, err
-	}
-	return p, nil
+func NewPerson(lang string, isFormatted bool, rnd translations.RandSource) (*Person, error) {
+	return generatePerson(lang, isFormatted, rnd)
 }
 
-// GeneratePerson generates a mock person with random data.
-// It returns a pointer to a Person and an error if the generation fails.
-func (p *Person) generatePerson(isFormatted bool) (*Person, error) {
-	lang := translations.GetLanguage()
+// GeneratePerson generates a mock person with random data
+// It returns a pointer to a Person and an error if the generation fails
+func generatePerson(lang string, isFormatted bool, rnd translations.RandSource) (*Person, error) {
+	firstNamesMale := translations.GetList(lang, "person_first_name_male")
+	firstNamesFemale := translations.GetList(lang, "person_first_name_female")
+	lastNames := translations.GetList(lang, "person_last_name")
 
-	// Get the list of first names and last names
-	firstNamesMale := strings.Split(translations.Get(lang, "person_first_name_male"), ",")
-	firstNamesFemale := strings.Split(translations.Get(lang, "person_first_name_female"), ",")
-	lastNames := strings.Split(translations.Get(lang, "person_last_name"), ",")
-
-	// Validate data
 	if len(firstNamesMale) == 0 || len(firstNamesFemale) == 0 {
-		return nil, fmt.Errorf("%w, %s", ErrNoFirstNames, translations.Translate("no_data_available_for_first_names"))
+		return nil, fmt.Errorf("%w, %s", ErrNoFirstNames, translations.Get(lang, "no_data_available_for_first_names"))
 	}
-
 	if len(lastNames) == 0 {
-		return nil, fmt.Errorf("%w, %s", ErrNoLastNames, translations.Translate("no_data_available_for_last_names"))
+		return nil, fmt.Errorf("%w, %s", ErrNoLastNames, translations.Get(lang, "no_data_available_for_last_names"))
 	}
 
-	// Choose random values
-	firstNameMale := firstNamesMale[rand.Intn(len(firstNamesMale))]
-	firstNameFemale := firstNamesFemale[rand.Intn(len(firstNamesFemale))]
-	lastName := lastNames[rand.Intn(len(lastNames))]
+	if rnd == nil {
+		rnd = rand.New(rand.NewSource(int64(rand.Int())))
+	}
 
-	// Generate a random gender
-	gender, err := gender.NewGender()
+	firstNameMale := firstNamesMale[rnd.Intn(len(firstNamesMale))]
+	firstNameFemale := firstNamesFemale[rnd.Intn(len(firstNamesFemale))]
+	lastName := lastNames[rnd.Intn(len(lastNames))]
+
+	genderVal, err := gender.NewGender(lang, rnd)
 	if err != nil {
 		return nil, err
 	}
 
-	// Generate a random CPF without a mask
-	cpf, err := cpf.NewCPF(isFormatted)
+	cpfVal, err := cpf.NewCPF(lang, isFormatted, rnd)
 	if err != nil {
 		return nil, err
 	}
 
-	// Validate required fields
 	if firstNameMale == "" || firstNameFemale == "" || lastName == "" {
-		return nil, fmt.Errorf("%w, %s", ErrGeneratingPerson, translations.Translate("error_generating_person"))
+		return nil, fmt.Errorf("%w, %s", ErrGeneratingPerson, translations.Get(lang, "error_generating_person"))
 	}
 
 	return &Person{
 		FirstNameMale:   firstNameMale,
 		FirstNameFemale: firstNameFemale,
 		LastName:        lastName,
-		Gender:          gender,
-		Age:             rand.Intn(70) + 18,
-		CPF:             cpf,
+		Gender:          genderVal,
+		Age:             rnd.Intn(70) + 18,
+		CPF:             cpfVal,
 	}, nil
 }
