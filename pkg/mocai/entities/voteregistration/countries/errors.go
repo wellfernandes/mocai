dd349dac4dfc37a7d08506219ef83ad1b4007e65
Welliton Fernandes Leal <wellfernandes@me.com>
2026-03-22 19:12:47 -0300

    refactor: removed unused errors file

--- pkg/mocai/entities/voteregistration/countries/errors.go
+++
@@ -1,9 +0,0 @@
-package countries
-
-import "errors"
-
-var (
-	ErrInvalidVoteRegistration = errors.New("vote registration: invalid vote registration")
-	ErrInvalidCheckDigit1      = errors.New("vote registration: invalid check digit 1")
-	ErrInvalidCheckDigit2      = errors.New("vote registration: invalid check digit 2")
-)
