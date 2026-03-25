77e2a11ae19d6bc8c70437cd32c4e94175e948c5
Welliton Fernandes Leal <wellfernandes@me.com>
2026-03-25 19:48:59 -0300

    refactor: adjusted national id generator and comments for docs

--- pkg/mocai/entities/nationalid/generator.go
+++ pkg/mocai/entities/nationalid/generator.go
@@ -5,16 +5,20 @@ import (
 	"github.com/brazzcore/mocai/pkg/mocai/translations"
 )
 
+// NationalID represents a national identity document.
 type NationalID struct {
 	BrazilianRG countries.RG
 }
 
-// NewNationalID generates a mock RG (Brazilian Identity Card) using a custom language and random source
+// NewNationalID generates a mock RG (Brazilian Identity Card) using a custom language and random source.
 func NewNationalID(lang string, isFormatted bool, rnd translations.RandSource) (*NationalID, error) {
 	return generateNationalID(lang, isFormatted, rnd)
 }
 
 func generateNationalID(lang string, isFormatted bool, rnd translations.RandSource) (*NationalID, error) {
-	rg := countries.NewRGCustom(isFormatted, rnd, lang)
+	rg, err := countries.NewRGCustom(lang, isFormatted, rnd)
+	if err != nil {
+		return nil, err
+	}
 	return &NationalID{BrazilianRG: rg}, nil
 }
