c0c8d64833663d5fb73b948eadf5f99ae73e438d
Welliton Fernandes Leal <wellfernandes@me.com>
2026-03-25 19:51:59 -0300

    refactor: cpf adjusted, sentinel errors added and comments for docs

--- pkg/mocai/entities/cpf/generator.go
+++ pkg/mocai/entities/cpf/generator.go
@@ -1,17 +1,22 @@
 package cpf
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 
 	"github.com/brazzcore/mocai/pkg/mocai/translations"
 )
 
+// ErrInvalidCPF is returned when a generated CPF is invalid.
+var ErrInvalidCPF = errors.New("cpf: invalid CPF")
+
+// CPF represents a Brazilian individual taxpayer registry number.
 type CPF struct {
 	Number string
 }
 
-// NewCPF generates a mock CPF using a custom language and random source
+// NewCPF generates a mock CPF using a custom language and random source.
 func NewCPF(lang string, isFormatted bool, rnd translations.RandSource) (*CPF, error) {
 	if rnd == nil {
 		rnd = translations.DefaultRandSource()
@@ -33,12 +38,15 @@ func NewCPF(lang string, isFormatted bool, rnd translations.RandSource) (*CPF, e
 	cpfNumber := strings.Trim(strings.Join(strings.Fields(fmt.Sprint(digits)), ""), "[]")
 	if isFormatted {
 		if len(cpfNumber) != 11 {
-			return nil, fmt.Errorf("%s", translations.Get(lang, "invalid_cpf"))
+			return nil, fmt.Errorf("%w: %s", ErrInvalidCPF, translations.Get(lang, "invalid_cpf"))
 		}
 		return &CPF{Number: cpfNumber[:3] + "." + cpfNumber[3:6] + "." + cpfNumber[6:9] + "-" + cpfNumber[9:]}, nil
 	}
 	return &CPF{Number: cpfNumber}, nil
 }
+
+// CalculateCheckDigit calculates a CPF check digit using the given weight.
+// This function is also used by the CPF validator.
 func CalculateCheckDigit(digits []int, weight int) int {
 	sum := 0
 	for _, digit := range digits {
