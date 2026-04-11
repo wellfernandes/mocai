a78190bc7b454e63413469243ad27da086ad867f
Welliton Fernandes Leal <wellfernandes@me.com>
2026-04-11 18:05:18 -0300

    docs(home): refactor homepage header and call-to-action link

--- mocai-docs/src/pages/index.js
+++ mocai-docs/src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className={clsx('button button--secondary button--lg', styles.customButton)}
-            to="/docs/tutorial">
+            to="/docs/documentation/get-started">
             Get Started with Mocaí
           </Link>
         </div>
